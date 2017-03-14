3d47ecbbedd0ba6fc146b3b5cabcac047d0068ce
AdminBiocomputo <ll.ferrer10@uniandes.edu.co>
2017-03-13 19:31:12 -0500

    Intento de Metropolis-Hastings

--- Monte-Carlo/expBayesEst.py
+++ Monte-Carlo/expBayesEst.py
@@ -27,7 +27,25 @@ def posterior(x, l):
         post[i] = verosimilitud(x,l[i])*prior(l[i])/evidencia(x)
     return post
 
-l = np.linspace(0.1,16,1000)
-prob = posterior(obs,l)
-plt.plot(l,prob)
+N = 10**6
+delta = 0.1
+lambda0 = 1.0
+lambdas = np.array([lambda0])
+
+def q(x1,x2): # Probabilidad de pasar a x1 dado que estoy en x2
+    return 1 # Vamos a asumir que hay la misma probabilidad de saltar a la derecha o a la izquierda
+
+def U(): # Un numero aleatorio que va a decidir si se da el salto o no
+    return np.random.rand()
+
+for i in range(N-2):
+    lambdanew = lambdas[i] + (np.random.rand() - 0.5)*2*delta # Posible posicion nueva
+    alpha = min(1,(posterior(obs,lambdanew)/posterior(obs,lambdas[i]))*(q(lambdanew,lambdas[i])/q(lambdas[i],lambdanew)))
+    u = U()
+    if (u < alpha):
+        lambdas = np.append(lambdas,lambdanew)
+    else:
+        lambdas = np.append(lambdas,lambdas[i])
+
+plt.hist(lambdas)
 plt.show()
