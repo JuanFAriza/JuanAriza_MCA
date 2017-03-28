f8d2247a6e698e61a50dd65d82667bff0bf9479a
AdminBiocomputo <ll.ferrer10@uniandes.edu.co>
2017-03-27 19:55:59 -0500

    En algún lugar hay algún error

---
+++ Machine_Learning/Minimos_Cuadrados/regression.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+N = 20
+
+datos = np.loadtxt('fake_regression.txt')
+
+training = datos[:int(0.75*len(datos))]
+test = datos[int(0.75*len(datos)):]
+
+x = training[:,0].copy()
+y = training[:,1].copy()
+
+x_test = test[:,0].copy()
+y_test = test[:,1].copy()
+
+Errores = np.array([])
+
+for i in range(1,N):
+    beta = np.polyfit(x,y,i)
+    y_est = np.zeros(len(x))
+    for j in range(i):
+        y_est = y_est + beta[j]*(x**(i-j))
+    err = (y - y_est)**2
+    ECP_tr = err.mean()
+    y_est = np.zeros(len(x_test))
+    for j in range(i):
+        y_est = y_est + beta[j]*(x_test**(i-j))
+    err = (y_test - y_est)**2
+    ECP_test = err.mean()
+
+    Error = ECP_tr + ECP_test
+    Errores = np.append(Errores, Error)
+
+print Errores
