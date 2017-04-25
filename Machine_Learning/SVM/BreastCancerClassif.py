823be8ccd23181bfcdeb7f53d5911bd937abef86
David Naranjo <df.naranjo10@uniandes.edu.co>
2017-04-24 19:52:37 -0500

    SVM

--- Machine_Learning/SVM/BreastCancerClassif.py
+++ Machine_Learning/SVM/BreastCancerClassif.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn import datasets
 from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 
 data = datasets.load_breast_cancer()
+print(data.DESCR)
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
